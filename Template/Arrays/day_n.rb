157c25fc8ccc4606d2a2e87d999e887116e029b7
Mark Glenn M. Gabuya <markglenn.g@gmail.com>
2024-09-18 17:02:09 +0800

    Started making the template for 10 Additional Ruby Array exercises.

---
+++ Template/Arrays/day_n.rb
@@ -0,0 +1,182 @@
+#* 1. Remove Duplicates
+puts "\n\n1. Remove Duplicates: "
+
+=begin
+  Write a method remove_duplicates that takes an array and returns a new array with duplicate
+  elements removed.
+=end
+
+
+#* Solution
+def remove_duplicates(array)
+
+
+
+end
+
+puts remove_duplicates([1, 2, 2, 3, 3, 4]) # => [1, 2, 3, 4]
+puts remove_duplicates(["a", "b", "a", "c"]) # => ["a", "b", "c"]
+puts remove_duplicates([1]) # => [1]
+
+
+
+#* 2. Flatten A Nested Array
+puts "\n\n2. Flatten A Nested Array: "
+
+=begin
+  Write a method flatten_array that takes a nested array and returns a new array where all
+  sub-arrays are flattened into one.
+=end
+
+
+#* Solution:
+def flatten_array(array)
+
+
+
+end
+
+puts flatten_array([[1, 2], [3, 4], [5]]) # => [1, 2, 3, 4, 5]
+puts flatten_array([[1], [2, [3, 4]], 5]) # => [1, 2, 3, 4, 5]
+puts flatten_array([1, 2, 3]) # => [1, 2, 3], already flat
+
+
+
+#* 3. Rotate Array Elements
+puts "\n\n3. Rotate Array Elements: "
+
+=begin
+  Write a method rotate_array that takes an array and an integer n, and rotates the array by n positions.
+  Positive n means rotating rightm negative n means rotating left.
+=end
+
+
+#* Solution
+def rotate_array(array, n)
+
+
+
+end
+
+puts rotate_array([1, 2, 3, 4, 5], 2) # => [4, 5, 1, 2, 3]
+puts rotate_array([1, 2, 3, 4, 5], -1) # => [2, 3, 4, 5, 1]
+puts rotate_array([1, 2, 3], 3) # => [1, 2, 3], full rotation
+
+
+
+#* 4. Find Unique Elements
+puts "\n\n"
+
+=begin
+  Write
+=end
+
+
+#* Solution
+def
+
+end
+
+puts
+
+
+
+#* 5.
+puts "\n\n"
+
+=begin
+  Write
+=end
+
+
+#* Solution
+def
+
+end
+
+puts
+
+
+
+#* 6.
+puts "\n\n"
+
+=begin
+  Write
+=end
+
+
+#* Solution
+def
+
+end
+
+puts
+
+
+
+#* 7.
+puts "\n\n"
+
+=begin
+  Write
+=end
+
+
+#* Solution
+def
+
+end
+
+puts
+
+
+
+#* 8.
+puts "\n\n"
+
+=begin
+  Write
+=end
+
+
+#* Solution
+def
+
+end
+
+puts
+
+
+
+#* 9.
+puts "\n\n"
+
+=begin
+  Write
+=end
+
+
+#* Solution
+def
+
+end
+
+puts
+
+
+
+#* 10.
+puts "\n\n"
+
+=begin
+  Write
+=end
+
+
+#* Solution
+def
+
+end
+
+puts
